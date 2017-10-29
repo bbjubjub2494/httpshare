11fe3773c51d72256fc49abf6b3bfa3057e45e1e
Julie B. <no-reply@bbjubjub.fr>
2017-10-29 22:18:53 +0100

    implement the executable archive

---
+++ make_zipapp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# make the ZIP Application for release.
+# This script uses the contents of the current commit. (NOT the worktree neither the index)
+# Incidentally, this script doesn't require any version of Python to be installed.
+
+(
+exec >httpshare.pyz
+echo '#!/usr/bin/env python'
+# we pass HEAD: instead of HEAD so that no file comment is added to the archive.
+# This is because Python doesn't want file comments in PyZ files.
+git archive --format=zip HEAD: '**.py' '**.stpl' LICENSE.txt
+)
+chmod +x httpshare.pyz
