87fff2adaa2a672c676d1040c53f395bcda6af97
Julie B. <no-reply@bbjubjub.fr>
2019-06-11 22:16:23 +0200

    fix Python 2

---
+++ src/httpshare/compat/urllib/parse.py
@@ -0,0 +1,10 @@
+# Copyright 2019 Julie Bettens
+
+
+import sys
+
+if sys.version_info < (3,):
+    from urlparse import ParseResult, SplitResult, parse_qs, parse_qsl, urldefrag, urljoin, urlparse, urlsplit, urlunparse, urlunsplit
+    from urllib import quote, quote_plus, unquote, unquote_plus, urlencode
+else:
+    from urllib.parse import ParseResult, SplitResult, parse_qs, parse_qsl, quote, quote_plus, unquote, unquote_plus, urldefrag, urlencode, urljoin, urlparse, urlsplit, urlunparse, urlunsplit
