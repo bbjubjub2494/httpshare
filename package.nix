391edb1080aa67817361678dbe769f00ce0d52c2
Julie B. <no-reply@bbjubjub.fr>
2019-10-15 19:08:47 +0200

    fix derivation for NixOS

--- package.nix
+++ package.nix
@@ -1,14 +1,15 @@
-{ stdenv, bats, python3, ensureNewerSourcesForZipFilesHook }:
+{ stdenv, bats, python3, ensureNewerSourcesForZipFilesHook, python }:
 stdenv.mkDerivation {
   name = "httpshare";
   src = ./.;
 
   buildInputs = [bats python3 ensureNewerSourcesForZipFilesHook];
+  propagatedBuildInputs = [python]; # need python (any version) for the shebang.
 
   dontPatchShebangs = true;  # Keep the portable shebang.
 
   buildPhase = ''
-    ./make_zipapp.py
+    ${python3}/bin/python make_zipapp.py
   '';
 
   installPhase = ''
