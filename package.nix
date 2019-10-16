c7f09a3a0cb44d58b3573e8810581c0aa815d58a
Julie B. <no-reply@bbjubjub.fr>
2019-10-16 22:22:42 +0200

    Lint nix expression

--- package.nix
+++ package.nix
@@ -1,33 +1,33 @@
 { pkgs }:
 let
   pyz = pkgs.callPackage ({ stdenv, bats, python3, ensureNewerSourcesForZipFilesHook }:
-stdenv.mkDerivation {
-  name = "httpshare.pyz";
-  src = ./.;
+    stdenv.mkDerivation {
+      name = "httpshare.pyz";
+      src = ./.;
 
-  buildInputs = [bats python3 ensureNewerSourcesForZipFilesHook];
+      buildInputs = [bats python3 ensureNewerSourcesForZipFilesHook];
 
-  dontPatchShebangs = true;  # Keep the portable shebang.
+      dontPatchShebangs = true;  # Keep the portable shebang.
 
-  buildPhase = ''
-    ${python3}/bin/python make_zipapp.py
-  '';
+      buildPhase = ''
+        ${python3}/bin/python make_zipapp.py
+      '';
 
-  installPhase = ''
-    cp httpshare.pyz $out
-  '';
-}) {};
+      installPhase = ''
+        cp httpshare.pyz $out
+      '';
+    }) {};
 in
 pkgs.callPackage ({ stdenv, python, makeWrapper }:
-stdenv.mkDerivation {
-  name = "httpshare";
-  inherit pyz;
+  stdenv.mkDerivation {
+    name = "httpshare";
+    inherit pyz;
 
-  buildInputs = [python makeWrapper];
+    buildInputs = [python makeWrapper];
 
-  phases = "installPhase";
-  installPhase = ''
-    mkdir -p $out/bin
-    makeWrapper ${python}/bin/python $out/bin/httpshare --add-flags $pyz
-  '';
-}) {}
+    phases = "installPhase";
+    installPhase = ''
+      mkdir -p $out/bin
+      makeWrapper ${python}/bin/python $out/bin/httpshare --add-flags $pyz
+    '';
+  }) {}
