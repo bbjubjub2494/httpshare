0d1cd0080dcbef55e6f32fe2df182d2b33adc37c
Julie B. <no-reply@bbjubjub.fr>
2019-10-16 23:14:21 +0200

    bump pinned Nixpkgs

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 let
   # use a pinned version of nixpkgs
-  nixpkgs-version = "19.03";
+  nixpkgs-version = "19.09";
   nixpkgs = (import (builtins.fetchTarball {
     # Descriptive name to make the store path easier to identify
     name = "nixpkgs-${nixpkgs-version}";
     url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-version}.tar.gz";
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "0q2m2qhyga9yq29yz90ywgjbn9hdahs7i8wwlq7b55rdbyiwa5dy";
+    sha256 = "0mhqhq21y5vrr1f30qd2bvydv4bbbslvyzclhw0kdxmkgg3z4c92";
   }) {}).extend (self: super: {
   });
 in
