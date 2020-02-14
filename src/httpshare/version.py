92c45cf84dd906d620eb2e77929ada9ab7f5c0dc
Julie B. <no-reply@bbjubjub.fr>
2020-02-14 22:39:26 +0100

    Nuke version prefixes, what even was I thinking?

--- src/httpshare/version.py
+++ src/httpshare/version.py
@@ -3,8 +3,7 @@
 
 from collections import namedtuple
 
-version_info = namedtuple('version', 'prefix major minor patch suffix')(
-    prefix='',
+version_info = namedtuple('version', 'major minor patch suffix')(
     major=1,
     minor=0,
     patch=7,
@@ -13,9 +12,5 @@ version_info = namedtuple('version', 'prefix major minor patch suffix')(
 
 version = '%(major)d.%(minor)d.%(patch)d' % version_info._asdict()
 
-if version_info.prefix:
-    version = '%s-%s' % (version_info.prefix, version)
-
 if version_info.suffix:
     version = '%s-%s' % (version, version_info.suffix)
-
