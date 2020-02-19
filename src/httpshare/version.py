5fdd9b2b7bd2492cb26724a17cbfbdee967cd835
Julie B. <no-reply@bbjubjub.fr>
2020-02-19 19:01:50 +0100

    Fix TypeError on some versions

--- src/httpshare/version.py
+++ src/httpshare/version.py
@@ -6,7 +6,7 @@ import json, pkgutil
 from collections import namedtuple
 
 version_info = namedtuple('version', 'major minor patch suffix')(
-    **json.loads(pkgutil.get_data('httpshare', 'version.json'))
+    **json.loads(pkgutil.get_data('httpshare', 'version.json').decode('u8'))
 )
 
 version = '%(major)d.%(minor)d.%(patch)d' % version_info._asdict()
